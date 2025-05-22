412e46fcf9fe622a4f576898e9c1586c453f39fc
ost1ck <kotula.ostap2003@gmail.com>
2025-05-22 20:36:17 +0300

    Use env variable for Firebase service account

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
